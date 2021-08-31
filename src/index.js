1d8c49af890836c66de225cd37716a65ef5e39c1
nguyendinhkhanh98 <nguyendinhkhanh20061998@gmail.com>
2021-09-01 00:14:08 +0700

    Refactor code: create router, app/controller foder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+//XMLhttpRequest, fetch, axios, 
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -17,13 +26,12 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Home, search, contact
+
+// routes init
+route(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)})
